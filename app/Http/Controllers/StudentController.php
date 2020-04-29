9f960cdf01ced69d8ebd64cf38ae9d1c30f497d9
Byukusenge Alphonse <alphonso2009@gmail.com>
2020-04-29 05:03:08 +0300

    Insert data from create page part 1

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Student;
 
 class StudentController extends Controller
 {
@@ -11,6 +12,21 @@ public function index(){
 }
     public function create(){
         return view('create');
+    }
+    public function store(Request $request){
+    $this->validate($request,[
+        'firstname' => 'required',
+        'lastname' => 'required',
+        'email' => 'required',
+        'phone' => 'required',
+    ]);
+    $student = new Student;
+        $student->first_name = $request->firstname;
+        $student->last_name = $request->lastname;
+        $student->email = $request->email;
+        $student->phone = $request->phone;
+        $student->save();
+        return redirect(route('home'))->with('successMsg','Student Successfully created');
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@
 
 Route::get('/', 'StudentController@index')->name('home');
 Route::get('/create', 'StudentController@create')->name('create');
-
+Route::post('/create', 'StudentController@store')->name('store');
 
 
 
